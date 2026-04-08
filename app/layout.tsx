1f73c9cb95e25dfad41bbc45c12200a780f6ec52
Gino Rubén Giorgi <ginorubengiorgi@gmail.com>
2026-04-08 17:06:23 -0300

    Bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
             <body
                 className="bg-[hsl(var(--deep-bg))] overflow-x-hidden"
                 suppressHydrationWarning
